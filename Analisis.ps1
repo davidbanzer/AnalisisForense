95de73204a46492f9e2842134d5d0a98f302f181
David <davidbanzer123@gmail.com>
2022-06-19 22:54:45 -0400

    sistema operativo

--- Analisis.ps1
+++ Analisis.ps1
@@ -21,7 +21,7 @@ function GetZonaHoraria {
     Write-Info "1. a) Zona Horaria"
     Write-Before ("*****")
     Get-TimeZone
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetFechaHora {
@@ -29,7 +29,7 @@ function GetFechaHora {
     Write-Info "1. a) Fecha y Hora"
     Write-Before ("*****")
     get-date
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetActualizaciones {
@@ -37,7 +37,7 @@ function GetActualizaciones {
     Write-Info "1. b) Actualizaciones"
     Write-Before ("*****")
     wmic qfe list
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetDireccionIP {
@@ -45,7 +45,7 @@ function GetDireccionIP {
     Write-Info "1. c) Direcciones IP"
     Write-Before ("*****")
     Get-NetIPAddress | Format-Table
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetTarjetasRed {
@@ -53,7 +53,7 @@ function GetTarjetasRed {
     Write-Info "1. c) Tarjetas de Red"
     Write-Before ("*****")
     Get-NetAdapter | Format-Table
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetNombreEquipo {
@@ -61,7 +61,7 @@ function GetNombreEquipo {
     Write-Info "1. d) Nombre del Equipo"
     Write-Before ("*****")
     hostname
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetUsuarios {
@@ -69,7 +69,7 @@ function GetUsuarios {
     Write-Info "1. d) Usuarios"
     Write-Before ("*****")
     Get-LocalUser | Format-Table
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetDominio {
@@ -77,7 +77,7 @@ function GetDominio {
     Write-Info "1. e) Dominio"
     Write-Before ("*****")
     Get-WMIObject Win32_ComputerSystem | Select-Object -ExpandProperty Domain
-    Write-After ("******")
+    Write-After ("*****")
 
 }
 
@@ -86,7 +86,7 @@ function GetCategoriasAuditoria {
     Write-Info "1. e) Categorias de Auditoria"
     Write-Before ("*****")
     auditpol /get /category:*
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetSubcategoriasAuditoria {
@@ -94,23 +94,31 @@ function GetSubcategoriasAuditoria {
     Write-Info "1. e) Subcategorias de Auditoria"
     Write-Before ("*****")
     auditpol /list /subcategory:*
-    Write-After ("******")
+    Write-After ("*****")
 }
 
 function GetSistemaArchivos {
     # Ver sistema de archivos
     Write-Info "4. a) Sistema de Archivos"
     Write-Before ("*****")
-    Get-Volume
-    Write-After ("******")
+    Get-Volume | Format-Table
+    Write-After ("*****")
 }
 
 function GetProgramasInstalados {
     # Ver programas instalados
     Write-Info "4. b) Programas Instalados"
     Write-Before ("*****")
-    Get-WmiObject -Class Win32_Product | Select-Object -Property Name
-    Write-After ("******")
+    Get-WmiObject -Class Win32_Product | Select-Object -Property Name 
+    Write-After ("*****")
+}
+
+function GetSistemaOperativo {
+    # Ver sistema operativo
+    Write-Info "4. c) Sistema Operativo"
+    Write-Before ("*****")
+    (Get-CimInstance -ClassName CIM_OperatingSystem).Caption
+    Write-After ("*****")
 }
 
 GetZonaHoraria
@@ -125,3 +133,4 @@ GetCategoriasAuditoria
 GetSubcategoriasAuditoria
 GetSistemaArchivos
 GetProgramasInstalados
+GetSistemaOperativo
